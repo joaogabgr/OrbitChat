ff52b9d780a3f3314d2c1391bb2c9333b46ece52
João Gabriel Grossi Solis <104585766+joaogabgr@users.noreply.github.com>
2023-10-22 14:27:37 -0300

    Melhoria na iniciação do projeto

--- app.py
+++ app.py
@@ -12,8 +12,5 @@ from routes.autenticar import *
 from routes.publicar import *
 from routes.perfil import *
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
